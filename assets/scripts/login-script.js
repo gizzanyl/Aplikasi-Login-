962f4a4d944cbe483fa08a04d2827e8c9a9df5eb
gizzanyl <jeonkootq@gmail.com>
2025-05-02 11:51:43 +0700

    Update login-script.js
    Mengganti komentar sesuai dengan format JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -4,22 +4,57 @@
  * Gunakan fungsi goToLogin() untuk menampilkan halaman login
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
-
+/**
+ * Membuat variabel loginFormElement untuk tampilan form.
+ * @constant {HTMLElement}
+*/
 const loginFormElement = document.querySelector('#loginForm');
+
+/**
+ * Membuat variabrl inputEmailElement untuk tampilan input email.
+ * @constant {HTMLElement}
+ */
 const inputEmailElement = document.querySelector('#inputEmail');
+
+/** 
+ * mambuat cariabel inputPasswordElement untuk tampilan input password.
+ * @constant {HTMLElement}
+*/
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/**
+ * membuat variabel expectedEmail untuk menyimpan informasi email sementara.
+ * @constant {string}
+ */
 const expectedEmail = 'admin@dicoding.com';
+
+/**
+ * membuat variabel expectedPassword untuk menyimpan informasi password sementara.
+ * @constant {string}
+ */
 const expectedPassword = 'superpassword';
 
+/* Comment : Menambahkan aksi klik pada button. */
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
+  /**
+     * membuat variabel email untuk menyimpan nilai email yang didapatkan saat button ditekan.
+     * @constant {string}
+     */
   const email = inputEmailElement.value;
+
+   /**
+     * membuat variabel password untuk menyimpan nilai password yang didapatkan saat button ditekan.
+     * @constant {string}
+     */
   const password = inputPasswordElement.value;
+   /* Comment : Memastikan bahwa nilai email dan password sesuai dengan nilai yang tersimpan. */
   if (email == expectedEmail && password == expectedPassword) {
+    /* comment : Jika sesuai maka program akan berpindah ke halaman home. */
     goToHome();
   } else {
+    /* Comment : Namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah. */
     showPopUp();
   }
 });
